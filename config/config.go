712fd046a8811cfeb4ae75ae67a0f4bd6d546711
Md. Pieash Ahmed <pieash9@gmail.com>
2025-06-06 22:51:56 +0600

    update

--- config/config.go
+++ config/config.go
@@ -2,12 +2,15 @@ package config
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnv() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file")
+	if os.Getenv("ENV") != "production" {
+		if err := godotenv.Load(); err != nil {
+			log.Println("Warning: no .env file found (likely running in production)")
+		}
 	}
 }
